68346b67b010f881cae8436c0e34d2894608c2d3
Jean Marinho <jeanmarinho529@gmail.com>
2024-06-03 19:41:48 -0300

    chore: adjust tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,19 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    './vendor/wireui/wireui/resources/**/*.blade.php',
+    './vendor/wireui/wireui/ts/**/*.ts',
+    './vendor/wireui/wireui/src/View/**/*.php'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  presets: [
+    require("./vendor/wireui/wireui/tailwind.config.js")
+  ],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
-
